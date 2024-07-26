63c2ad35d640cdac9ff7175eebdb4819126b60d3
med6ba <mohamedbenabdessadak57@gmail.com>
2024-07-27 00:58:47 +0100

    first repo :)

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -21,13 +21,9 @@ void	ft_putchar(char c)
 void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
-	{
 		write (1, "-2147483648", 11);
-	}
 	else if (nb >= 0 && nb <= 9)
-	{
 		ft_putchar(nb);
-	}
 	else if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
